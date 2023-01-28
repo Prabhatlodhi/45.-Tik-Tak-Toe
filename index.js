ee545489b975635967f06982d030af6df6997ee3
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-01-28 12:48:24 +0530

    aa

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ function nextPage (e) {
     e.preventDefault();
     let nOne = FirstName.value;
     let nTwo = NameTwo.value;
-    if( nOne == "" ){
-        console.log('555')
+    if( nOne == "" && nTwo == "" ){
+        // console.log('555')
         // console.log(nOne)
-        alert("Please Enter Player One Name")
-    } else if( nTwo == "" ){
-        console.log('444')
-        alert( "Please Enter Player Two Name")
-    }else{
-        
-
-        localStorage.setItem('playerOneName', nOne )
-        localStorage.setItem('playerTwoName', nTwo )
-        window.location.href = "page.html"
+        // alert("Please Enter Player One Name")
+        nOne = 'Player 1'
+        nTwo = 'Player 2'
+    } else if( nOne == "" || nOne == " "){
+        // console.log('444')
+        // alert( "Please Enter Player Two Name")
+        nOne = 'Player 1'
+    }else if( nTwo == "" || nTwo == " ")  {
+        nTwo = 'Player 2'
     }
+    localStorage.setItem('playerOneName', nOne )
+    localStorage.setItem('playerTwoName', nTwo )
+    window.location.href = "page.html"
 }
